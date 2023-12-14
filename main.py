5cd260ec48f4c15079d36ce5233156e299debfd9
Soulbolt <soulbolt@gmail.com>
2023-12-14 10:18:08 -0700

    Added functions to allow user to move around to create a drawing, clear function to reset,bound to a,w,s,d and c keys.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-
+# Create an Etch_a_sketch app
 ralph = Turtle()
 screen = Screen()
 
@@ -8,6 +8,31 @@ def move_forwards():
     ralph.forward(10)
 
 
-screen.listen() # Listen for a key action, in this case the space bar key
-screen.onkey(key="space", fun=move_forwards)  # When calling a function in here, the () is not needed
+def move_backwards():
+    ralph.backward(10)
+
+
+def turn_left():
+    new_heading = ralph.heading() + 10
+    ralph.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = ralph.heading() - 10
+    ralph.setheading(new_heading)
+
+
+def clear():
+    ralph.clear()
+    ralph.penup()
+    ralph.home()
+    ralph.pendown()
+
+
+screen.listen()  # Listen for a key action
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")  # When calling a function in here, the () is not needed
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
 screen.exitonclick()
